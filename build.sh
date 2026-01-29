125ea6114dca7d5cf2eff431821a3197987e9a9f
홍요한 <ghddygks45@gmail.com>
2026-01-29 18:25:29 +0900

    chore: 버셀 배포를 위한 파일 생성

---
+++ build.sh
@@ -0,0 +1,7 @@
+// ./build.sh 
+
+#!/bin/sh
+cd ../
+mkdir output
+cp -R ./coworkers/* ./output
+cp -R ./output ./coworkers/
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={}>
-        <>
-          <h1 className="text-3xl font-bold text-red-500 underline">
-            Hello world!
-          </h1>
-        </>
-      </Route>
-    </Routes>
-  );
+  return <div>Hello world!</div>;
 }
 
 export default App;
